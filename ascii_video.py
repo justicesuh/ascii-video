5dbfc566754095e929a51060e56411f562510ad1
Justice Suh <justice.suh@gmail.com>
2018-09-26 23:13:24 -0400

    Resize image

--- ascii_video.py
+++ ascii_video.py
@@ -9,12 +9,15 @@ if __name__ == '__main__':
     image = Image.open(filename).convert('L')
     width, height = image.size
 
+    ascii_width = 80
+    ascii_height = int(ascii_width / width * height)
+    print(Image.NEAREST)
+    image = image.resize((ascii_width, ascii_height), Image.LANCZOS)
+
     ascii_art = ''
-    for h in range(height):
-        for w in range(width):
-            pixel = image.getpixel((w, h))
-            index = int(pixel * (len(grayscale) - 1) / 255)
-            ascii_art += grayscale[index]
+    for h in range(ascii_height):
+        for w in range(ascii_width):
+            ascii_art += grayscale[int(image.getpixel((w, h)) * 69 / 255)]
     
-    for i in range(0, len(ascii_art), width):
-        print(ascii_art[i:i + width])
+    for i in range(0, len(ascii_art), ascii_width):
+        print(ascii_art[i:i + ascii_width])
